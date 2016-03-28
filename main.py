807ecd20e303dd38100df45ee0d9f65aa65ec8e9
Shen <etkids@163.com>
2016-03-28 10:27:44 -0400

    refactored, add IP forgery, running forever

--- main.py
+++ main.py
@@ -1,44 +1,75 @@
-# coding: utf-8
-import requests
-from random import shuffle
+# -*- coding: utf-8 -*-
 from faker import Factory
+import requests
+import random
 
 fake = Factory.create()
 
-username = list(filter(str.isalpha, fake.name()))
-username = ''.join(username)
-email = fake.email()
-realname = Factory.create(locale='zh_CN').name()
 
-register_data = {
-    "userName": username,
-    "email": email,
-    "password": "123456",
-    "password1": "123456",
-    "realName": realname,
-    "province": "北京市",
-    "job": "IT|互联网|通信",
-    "mobile": "",
-    "address": "",
-}
+def headers():
+    return {
+        'X-Forwarded-For': ip()
+    }
 
-register_data = {
-    '_ZVING_METHOD': 'Register.doRegister',
-    '_ZVING_URL': '%2Fregister',
-    '_ZVING_DATA': str(register_data),
-    '_ZVING_DATA_FORMAT': 'json',
-}
 
-url = "http://zqyj.chinalaw.gov.cn/ajax/invoke"
+def ip():
+    return '.'.join([str(x) for x in [random.randint(1, 254),
+                                      random.randint(1, 254),
+                                      random.randint(1, 254),
+                                      random.randint(1, 254)]])
 
-r1 = requests.post(url, data=register_data)
-print(r1.status_code, r1.text)
 
-vote_data = {
-    '_ZVING_METHOD': 'SupportOppose.voteOppose',
-    '_ZVING_URL': '%2FdraftDetail',
-    '_ZVING_DATA': '{"ID":"45114"}',
-    '_ZVING_DATA_FORMAT': 'json',
-}
-r2 = requests.post(url, data=vote_data, cookies=r1.cookies)
-print(r2.status_code, r2.text)
+def vote():
+    username = fake.user_name()
+    email = fake.email()
+    real_name = Factory.create(locale='zh_CN').name()
+
+    s = requests.session()
+    s.headers.update(headers())
+
+    register_data = {
+        "userName": username,
+        "email": email,
+        "password": "123456",
+        "password1": "123456",
+        "realName": real_name,
+        "province": "北京市",
+        "job": "IT|互联网|通信",
+        "mobile": "",
+        "address": "",
+    }
+
+    register_data = {
+        '_ZVING_METHOD': 'Register.doRegister',
+        '_ZVING_URL': '%2Fregister',
+        '_ZVING_DATA': str(register_data),
+        '_ZVING_DATA_FORMAT': 'json',
+    }
+
+    url = "http://zqyj.chinalaw.gov.cn/ajax/invoke"
+
+    r1 = s.post(url, data=register_data)
+    print(r1.status_code, r1.text)
+
+    vote_data = {
+        '_ZVING_METHOD': 'SupportOppose.voteOppose',
+        '_ZVING_URL': '%2FdraftDetail',
+        '_ZVING_DATA': '{"ID":"45114"}',
+        '_ZVING_DATA_FORMAT': 'json',
+    }
+    r2 = s.post(url, data=vote_data, cookies=r1.cookies)
+    print(r2.status_code, r2.text)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--count", help="vote how many times", default=10, type=int)
+    parser.add_argument("--forever", help="vote forever", action='store_true')
+    args = parser.parse_args()
+    if args.forever:
+        while True:
+            vote()
+    else:
+        for i in range(args.count):
+            vote()
