90d74271a193adb443cb298eff8597ed13eac0eb
Shen <etkids@163.com>
2016-03-28 21:50:19 -0400

    update bash script

--- main.sh
+++ main.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-SCRIPT_URL=https://raw.githubusercontent.com/loggerhead/vote_against_37/master/main.py
+if [ -x "$(which pip)" ]; then
+        echo '---'
+else
+        echo 'installing pip'
+        curl -L 'https://bootstrap.pypa.io/get-pip.py' | python -
+fi
 
-pip3 install fake-factory requests
-curl -L $SCRIPT_URL | python3 -
+SCRIPT_URL=https://raw.githubusercontent.com/ericls/vote_against_37/master/main.py
+
+python -m pip install fake-factory requests
+curl -L $SCRIPT_URL | python - --forever --timeout 3
